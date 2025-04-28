398c0d9caa1bf14ca7c9f513056d5d0d437a5675
Bruno Leal <brunofilizolaleal@gmail.com>
2025-04-28 16:48:55 -0300

    Add initial interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SectionComponent } from "./section/section.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [RouterOutlet, NavbarComponent, SectionComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
