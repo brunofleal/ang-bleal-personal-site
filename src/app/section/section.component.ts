6a67fb9755b4e9ec8cbda76c335736d15038e0d6
Bruno Leal <brunofilizolaleal@gmail.com>
2025-05-05 11:53:51 -0300

    add basic animation

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,12 +1,40 @@
 import { Component, Input } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations'
+import { InViewportModule } from 'ng-in-viewport';
+
+
+const hidden = { opacity:0 };
+const visible = { opacity:1 };
+const animationDuration = 1;
+const animation = `${animationDuration}s ease-in`;
 
 @Component({
   selector: 'app-section',
-  imports: [],
+  imports: [
+    InViewportModule
+  ],
   templateUrl: './section.component.html',
-  styleUrl: './section.component.css'
+  styleUrl: './section.component.css',
+  animations: [
+    trigger('visibleHidden', [
+      transition(':enter', [
+        style(hidden),
+        animate(animation), style(visible)
+      ]),
+      transition(':leave', [
+        style(visible),
+        animate(animation), style(hidden)
+      ]),
+    ])
+  ]
 })
+
 export class SectionComponent {
   @Input({ required: false }) imagePath!: string;
   @Input({ required: false }) toHref!: string;
+  protected isVisible: boolean = false;
+
+  isInViewPort (event:any) {
+    this.isVisible = event.target.visible;
+  }
 }
